0ed0751b73e58ec6343c392daec5b809973b9aed
Youngjju <2000dudwn@naver.com>
2025-04-25 20:04:52 +0900

    feat: update MajorGraph component styles and maintain major number logging

--- components/Project/ProjectField/MajorGraph.tsx
+++ components/Project/ProjectField/MajorGraph.tsx
@@ -7,6 +7,7 @@ interface MajorGraphProps {
 
 export default function MajorGraph({ project }: MajorGraphProps) {
   const majorNumber = project.majors.length;
+  console.log(majorNumber);
   return (
     <div className="w-full h-[152px] shadow-md rounded-lg p-1">
       <div className="w-full h-auto justify-between flex">
@@ -26,10 +27,22 @@ export default function MajorGraph({ project }: MajorGraphProps) {
         ))}
       </div>
       <div className="w-full h-4 rounded-2xl relative justify-between ">
-        <div className="w-1/4 left-0  absolute z-40 h-full bg-amber-500 rounded-2xl"></div>
-        <div className="w-1/2 left-0 absolute z-30 h-full bg-blue-400 rounded-2xl"></div>
-        <div className="w-3/4 left-0 absolute z-20 h-full bg-emerald-400 rounded-2xl"></div>
-        <div className="w-full h-full z-10 bg-red-500 rounded-2xl"></div>
+        <div className="w-1/4 left-0  absolute z-40 h-full bg-secondary-100 rounded-2xl"></div>
+        <div
+          className={`${
+            majorNumber >= 2 ? `` : `hidden`
+          }  w-1/2 left-0 absolute z-30 h-full bg-blue-300 rounded-2xl`}
+        ></div>
+        <div
+          className={` ${
+            majorNumber >= 3 ? `` : ` hidden`
+          } w-3/4 left-0 absolute z-20 h-full bg-yellow-300 rounded-2xl`}
+        ></div>
+        <div
+          className={` ${
+            majorNumber == 4 ? `` : `hidden`
+          }  w-full h-full z-10 bg-fuchsia-400  rounded-2xl`}
+        ></div>
         <div className="w-full h-full top-0 left-0  -z-10 bg-gray-100 absolute"></div>
       </div>
     </div>
