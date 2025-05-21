2e5f536ca7cd44a3c17f0c2856ee8be2b3d4baf3
GalaxyBook2 <manamana32321@gmail.com>
2025-05-21 20:53:49 +0900

    refactor: improve className handling in ProjectTextArea component

--- app/projects/[id]/_components/ProjectTextArea.tsx
+++ app/projects/[id]/_components/ProjectTextArea.tsx
@@ -1,5 +1,6 @@
 import { ProjectPageMode, ProjectPageModeEnum } from "@/app/projects/[id]/page";
 import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
 
 interface ProjectTextAreaProps {
@@ -10,15 +11,16 @@ interface ProjectTextAreaProps {
 }
 
 const ProjectTextArea = forwardRef<HTMLTextAreaElement, ProjectTextAreaProps>(
-  ({ title, value, onChange, mode = false }, ref) => {
+  ({ title, value, onChange, mode }, ref) => {
     return (
       <div className="w-full h-auto">
         <div className="w-full text-lg font-semibold">{title}</div>
         <Textarea
           ref={ref}
-          className={`w-full resize-none mt-2 border p-2 rounded  ${
-            mode ? "bg-gray-100" : "bg-white"
-          }`}
+          className={cn(
+            "w-full resize-none mt-2 border p-2 rounded",
+            mode === ProjectPageModeEnum.ADMIN ? "bg-gray-100" : "bg-white"
+          )}
           value={value}
           onChange={mode === ProjectPageModeEnum.ADMIN ? onChange : undefined}
           readOnly={mode === null}
