2dafc3af0da6277221d74a5951c8bb5bbac2092b
Youngjju <2000dudwn@naver.com>
2025-05-21 11:03:46 +0900

    feat: update AppSidebar component to use correct URLs and improve layout

--- components/Header/AppSidebar.tsx
+++ components/Header/AppSidebar.tsx
@@ -7,10 +7,12 @@ import {
   SidebarMenuItem,
   SidebarMenuButton,
 } from "@/components/ui/sidebar";
+import { X } from "lucide-react";
+import Link from "next/link";
 
 const items = [
-  { title: "프로젝트", url: "#" },
-  { title: "공지사항", url: "#" },
+  { title: "프로젝트", url: "/" },
+  { title: "공지사항", url: "/notice" },
 ];
 
 export function AppSidebar() {
@@ -20,12 +22,18 @@ export function AppSidebar() {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
+              <div className="w-full h-14 flex mb-5 items-center justify-end">
+                <X className="size-12 stroke-1" />
+              </div>
               {items.map((item) => (
-                <SidebarMenuItem className="h-12" key={item.title}>
+                <SidebarMenuItem className="h-14" key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url} className="flex items-center gap-2">
-                      <span>{item.title}</span>
-                    </a>
+                    <Link
+                      className="flex justify-end h-full items-center gap-2"
+                      href={item.url}
+                    >
+                      <span className="text-2xl">{item.title}</span>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
