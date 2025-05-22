cb8a5e7163fbc9c4146bbff1b3fd794ce294d16e
Youngjju <2000dudwn@naver.com>
2025-05-22 16:50:07 +0900

    feat: update PostPage layout and structure for improved readability

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,9 +1,22 @@
+import { Calendar, Eye } from "lucide-react";
+
 export default function PostPage() {
   return (
-    <div className="min-w-screen flex justify-center h-auto">
-      <div className="pageWidth pt-12">
+    <div className="pageWidth w-full flex justify-center h-auto">
+      <div className=" pt-12">
         <div className="h-16 text-4xl font-medium pb-2 flex flex-col justify-end border-b-[1px] border-black">
           팀 빌딩 시 주의사항
+        </div>
+        <div>
+          <span>김훈모</span>
+          <div>
+            <Eye />
+            <span>1234</span>
+          </div>
+          <div>
+            <Calendar />
+            <span> 2025-02-06 </span>
+          </div>
         </div>
       </div>
     </div>
