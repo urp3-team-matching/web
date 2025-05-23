0b17f7587ce0da3067ca973ef7cce81bf0aadf3f
GalaxyBook2 <manamana32321@gmail.com>
2025-05-23 17:09:55 +0900

    fix: update font variable and improve body class assignment in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 import Header from "@/components/Header";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-export const pretendard = Noto_Sans_KR({
+export const notoSansKR = Noto_Sans_KR({
   variable: "--font-noto-sans-kr",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "융합연구학점제 팀 모집 플랫폼",
   description: "성균관대학교 융합연구학점제 팀 모집 플랫폼",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${pretendard.variable} flex flex-col items-center antialiased`}
-      >
+      <body className={cn(notoSansKR.className, "flex flex-col items-center")}>
         <NuqsAdapter>
           <Header />
           <div className="container">{children}</div>
