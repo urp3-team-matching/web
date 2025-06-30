671a6c0d7263d07faf4eefe88d0319c7fa1218dd
GalaxyBook2 <manamana32321@gmail.com>
2025-06-30 17:29:52 +0900

    feat: supabase 클라이언트에 환경에 따른 스키마 설정 추가

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,8 @@ import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey, {
+  db: {
+    schema: process.env.NODE_ENV === "production" ? "public" : "dev", // 프로덕션 환경에서는 public 스키마 사용
+  },
+});
