53c36b59834b63677aa5154b0b2ac0be50bc85ad
Youngjju <2000dudwn@naver.com>
2025-07-05 19:45:48 +0900

    chore: 모바일에서의 키워드 뱃지를 정리하기 위해 만든 키워드 메뉴바를 컴포넌트화

--- app/projects/[id]/_components/Header/index.tsx
+++ app/projects/[id]/_components/Header/index.tsx
@@ -7,16 +7,9 @@ import ProjectNameForm from "@/components/Project/Form/ProjectNameForm";
 import { PublicProjectWithForeignKeys } from "@/lib/apiClientHelper";
 import { parseDate } from "@/lib/utils";
 import { ProjectInput } from "@/types/project";
-import { Calendar, ChevronDown, Eye } from "lucide-react";
+import { Calendar, Eye } from "lucide-react";
 import { Control } from "react-hook-form";
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
-import { Badge } from "@/components/ui/badge";
+import KeywordMenubar from "./KeywordMenubar";
 
 interface ProjectDetailHeaderProps {
   project: PublicProjectWithForeignKeys;
@@ -50,29 +43,7 @@ const ProjectDetailHeader = ({
               ))}
             </div>
           )}
-          {mode === null && (
-            <Menubar className="lg:hidden h-full p-0 border-none bg-none shadow-none">
-              <MenubarMenu>
-                <MenubarTrigger className="w-full p-0 h-6 sm:h-7 border-none">
-                  <Badge className="sm:w-[72px] bg-gray-200 text-black w-14 h-6 text-[11px] p-0 sm:h-7 font-medium sm:text-sm rounded-sm">
-                    키워드
-                    <ChevronDown size={16} />
-                  </Badge>
-                </MenubarTrigger>
-                <MenubarContent>
-                  <MenubarItem>
-                    {mode === null && (
-                      <div className="w-auto h-full flex gap-1 items-center">
-                        {project.keywords.map((keyword) => (
-                          <KeywordBadge key={keyword} keyword={keyword} />
-                        ))}
-                      </div>
-                    )}
-                  </MenubarItem>
-                </MenubarContent>
-              </MenubarMenu>
-            </Menubar>
-          )}
+          {mode === null && <KeywordMenubar mode={mode} project={project} />}
         </div>
 
         <div className="flex gap-x-2 items-center">
