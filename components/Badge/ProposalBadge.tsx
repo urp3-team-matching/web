d94ebec8712c4569bf8ac7c2efcef4a6f5947a99
Youngjju <2000dudwn@naver.com>
2025-07-26 17:24:39 +0900

    chore: 제안자 유형에 따라 뱃지 색상 바뀌도록 설정, 팀원 확정현황 위아래 가운데 정렬 + 디자인 반영

--- components/Badge/ProposalBadge.tsx
+++ components/Badge/ProposalBadge.tsx
@@ -1,5 +1,6 @@
 import { ProposerType } from "@prisma/client";
 import { Badge } from "../ui/badge";
+import { cn } from "@/lib/utils";
 
 interface ProposalBadgeProps {
   proposerType: ProposerType;
@@ -7,24 +8,33 @@ interface ProposalBadgeProps {
 
 export default function ProposalBadge({ proposerType }: ProposalBadgeProps) {
   let proposerText = "";
-
+  let badgeColor = "";
   switch (proposerType) {
     case ProposerType.PROFESSOR:
       proposerText = "교수제안";
+      badgeColor = "bg-third";
       break;
     case ProposerType.STUDENT:
       proposerText = "학생제안";
+      badgeColor = "bg-teal-800";
       break;
     case ProposerType.HOST:
       proposerText = "융합원제안";
+      badgeColor = "bg-indigo-800";
       break;
     default:
       proposerText = "제안자 없음";
+      badgeColor = "bg-gray-400";
       break;
   }
 
   return (
-    <Badge className="sm:w-[72px] sm:h-7 w-14 h-6 text-[11px] rounded-sm flex justify-center items-center bg-third sm:text-sm font-medium text-white">
+    <Badge
+      className={cn(
+        "sm:w-[72px] sm:h-7 w-14 h-6 text-[11px] rounded-sm flex justify-center items-center sm:text-sm font-medium text-white",
+        badgeColor
+      )}
+    >
       {proposerText}
     </Badge>
   );
