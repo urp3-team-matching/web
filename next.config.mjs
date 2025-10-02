06112047b04accf26657b64106484d151050ad8e
GalaxyBook2 <manamana32321@gmail.com>
2025-10-03 00:15:36 +0900

    feat: add OpenTelemetry instrumentation support

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     unoptimized: process.env.NODE_ENV === 'development',
   },
+  experimental: {
+    instrumentationHook: true,
+  }
 };
 
 export default nextConfig;
